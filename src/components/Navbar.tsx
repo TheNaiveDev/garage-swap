5fd81fd271e1f57cec99b6e06fb052b4075909f9
Denzel <bugsummorner@gmail.com>
2026-03-03 06:07:01 +0000

    navbar search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "./shared/Button";
-import Input from "./shared/input";
+import Input from "./shared/Input";
 import { RiSearchLine } from "@remixicon/react";
 
 export default function Navbar() {
